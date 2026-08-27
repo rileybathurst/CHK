68237dfc8fd023ba4cd771147a5762982272ddef
Riley Bathurst <riley@rileybathurst.com>
2026-08-27 13:06:33 +1200

    decleration pies

---
+++ inc/decleration-pies.php
@@ -0,0 +1,31 @@
+<?php
+// PIES
+function prefix_admin_declarationPies() {
+
+	// Extremely Important to set
+			global $wpdb;
+
+			// Whats inserted
+			$wpdb->insert( 'meatorders' ,
+				array(
+					'unid'          => '$unid' ,
+					'animal'        => '' ,
+					'declaration'   => $_POST['declaration'] ,
+				) , 
+				array(
+					'%s' ,
+					'%s' ,
+					'%s'
+				) 
+			);
+	
+			// this give the unid in the next url
+			$id = $wpdb->insert_id;
+			
+			// Redirect
+			wp_redirect( home_url() . '/order-details?n=' . $id );
+			exit();
+
+}
+add_action( 'admin_post_declarationPies', 'prefix_admin_declarationPies' );
+add_action( 'admin_post_nopriv_declarationPies', 'prefix_admin_declarationPies' );
